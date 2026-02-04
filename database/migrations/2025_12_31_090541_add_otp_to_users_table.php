1a68e003c0d451b66a88caa53304c0f330761392
ESNyarobi123 <ezekielsalehe00@gmail.com>
2026-02-04 14:46:34 +0300

    feat: Implement initial database schema including users, cache, jobs, sessions, password resets, payment settings, and OTP for users.

--- database/migrations/2025_12_31_090541_add_otp_to_users_table.php
+++ database/migrations/2025_12_31_090541_add_otp_to_users_table.php
@@ -4,16 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('otp')->nullable();
-            $table->timestamp('otp_expires_at')->nullable();
+            if (!Schema::hasColumn('users', 'otp')) {
+                $table->string('otp')->nullable();
+            }
+            if (!Schema::hasColumn('users', 'otp_expires_at')) {
+                $table->timestamp('otp_expires_at')->nullable();
+            }
         });
     }
 
